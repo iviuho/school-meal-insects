3d1c0a1580604b78c47821e9ea59df4d3ef2a8a1
Mangbo <whdudghks45629@gmail.com>
2019-06-24 12:20:20 +0900

    move data page
    success just move

--- fe/src/views/meal.vue
+++ fe/src/views/meal.vue
@@ -6,11 +6,11 @@
       <v-card>
         <v-card-title primary-title v-for="breakfs in breakfast">
           <div>
-            <h3 @click="test()" >{{breakfs}}</h3>
-            <v-btn flat icon @click="test()">
+            <h3 @click="test(breakfs)" >{{breakfs}}</h3>
+            <v-btn flat icon @click="postReq(breakfs, 'like')">
               <v-icon>thumb_up</v-icon>
             </v-btn>
-            <v-btn flat icon>
+            <v-btn flat icon @click="postReq(breakfs, 'dislike')">
               <v-icon>thumb_down</v-icon>
             </v-btn>
           </div>
@@ -23,6 +23,8 @@
 </template>
 
 <script>
+import router from "../router"
+
 export default {
   data: function () {
     return {
@@ -35,27 +37,39 @@ export default {
     this.getUsers()
   },
   methods: {
-    // getUsers () {
-    //   // using JSONPlaceholder
-    //   const baseURI = 'http://10.120.73.216:3000/meal';
-    //   this.$http.get(`${baseURI}`)
-    //   .then((result) => {
-    //     console.log(result)
-    //     console.log(result.data.data)
-    //     console.log(result.data.data.breakfast)
-    //     this.posts = result.data
-    //     console.log(this.posts.data.breakfast)
-    //     console.log(this.posts)
-    //     console.log(this.posts.data.breakfast.length)
-    //     this.leng = this.posts.data.breakfast.length
-    //     this.breakfast = this.posts.data.breakfast
-    //   })
-    //   .catch((e) => {
-    //     console.error(e.message)
-    //   })
-    // },
-    test() {
-      console.log("test")
+    getUsers () {
+      // using JSONPlaceholder
+      const baseURI = 'http://10.120.73.216:3000/meal';
+      this.$http.get(`${baseURI}`)
+      .then((result) => {
+        console.log(result)
+        console.log(result.data.data)
+        console.log(result.data.data.breakfast)
+        this.posts = result.data
+        console.log(this.posts.data.breakfast)
+        console.log(this.posts)
+        console.log(this.posts.data.breakfast.length)
+        this.leng = this.posts.data.breakfast.length
+        this.breakfast = this.posts.data.breakfast
+      })
+      .catch((e) => {
+        console.error(e.message)
+      })
+    },
+    test(menu) {
+      this.$router.push('menu/'+menu)
+    },
+    postReq(name, order) {
+      const baseURI = 'http://10.120.73.216:3000/menu/';
+      this.$http.post(`${baseURI + name}`, {
+        order: order
+      })
+      .then((r) => {
+        console.log("성공")
+      })
+      .catch((e) => {
+        console.error(e.message)
+      })
     }
   }
 }
