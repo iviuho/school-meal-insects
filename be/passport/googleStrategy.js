8275b5f7358bc7152d0c625d33b57b3930840986
MunJH <zero4125966@gmail.com>
2019-06-26 17:07:47 +0900

    구글 로그인 추가 2

--- be/passport/googleStrategy.js
+++ be/passport/googleStrategy.js
@@ -1,10 +1,10 @@
+const passport = require("passport");
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-require("dotenv").config();
 
-var GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-var GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+var GOOGLE_CLIENT_ID = "945992092243-4nr84drg218d8l795mkb84qs2sv7j640.apps.googleusercontent.com";
+var GOOGLE_CLIENT_SECRET = "zkgOLL9dJvWQIp4NYCSfGGtR";
 
-module.exports = (passport) => {
+module.exports = () => {
     passport.serializeUser((user, done) => {
         done(null, user);
     });
@@ -19,8 +19,9 @@ module.exports = (passport) => {
         callbackURL: 'http://localhost:3000/auth/google/callback',
 
     }, function (accessToken, refreshToken, profile, done) {
-        const socialId = profile.id;
-        const nickname = profile.displayName;
-        const profileImageUrl = profile.photos[0].value;
+        // const socialId = profile.id;
+        // const nickname = profile.displayName;
+        // const profileImageUrl = profile.photos[0].value;
+        console.log(profile);
     }));
 };
