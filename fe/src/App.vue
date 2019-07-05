a63c94cc30553db1597585bd479c3297a798a3eb
IVIuho <eogh2811@naver.com>
2019-07-05 18:54:19 +0900

    회원가입 및 로그아웃 폼 추가

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -3,7 +3,7 @@
       <v-navigation-drawer permanent app>
         <v-toolbar flat>
           <v-list>
-            <v-list-tile @click="goHome">
+            <v-list-tile @click="$router.push('/')">
               <v-list-tile-title class="title">
                 급식충들 모여라
               </v-list-tile-title>
@@ -15,9 +15,9 @@
 
         <v-toolbar flat class="transparent">
           <v-list class="pa-0">
-            <v-list-tile avatar @click="test" v-if="isAuth">
+            <v-list-tile avatar @click="logoutDialog = true" v-if="isAuth">
               <v-list-tile-avatar>
-                <img src="https://randomuser.me/api/portraits/men/85.jpg">
+                <v-icon>done</v-icon>
               </v-list-tile-avatar>
 
               <v-list-tile-content>
@@ -25,7 +25,7 @@
               </v-list-tile-content>
             </v-list-tile>
 
-            <v-list-tile avatar @click="dialog = true" v-else>
+            <v-list-tile avatar @click="loginDialog = true" v-else>
               <v-list-tile-avatar>
                 <v-icon>person</v-icon>
               </v-list-tile-avatar>
@@ -57,11 +57,23 @@
       </v-navigation-drawer>
     <v-content style="background-color: #FAFAFA;">
       <router-view/>
-      <v-dialog v-model="dialog" max-width="600px">
+
+      <v-dialog v-model="logoutDialog" max-width="600px">
         <v-card>
-          <v-card-title>
-            <span class="headline">회원 로그인</span>
-          </v-card-title>
+          <v-card-title class="headline">로그아웃</v-card-title>
+          <v-card-text>로그아웃 하시겠습니까?</v-card-text>
+
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn color="red lighter-1" dark @click="logoutDialog = false">취소</v-btn>
+            <v-btn color="green lighter-1" dark @click="toLogout">로그아웃</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="loginDialog" max-width="600px">
+        <v-card>
+          <v-card-title class="headline">로그인</v-card-title>
           <v-card-text>
             <v-container grid-list-md>
               <v-layout wrap>
@@ -74,17 +86,47 @@
                   :type="pwToggle ? 'text' : 'password'"
                   @click:append="pwToggle = !pwToggle"></v-text-field>
                 </v-flex>
-                계정없는 흑우 없제?
               </v-layout>
             </v-container>
           </v-card-text>
           <v-card-actions>
-            <v-spacer></v-spacer>
+            <v-btn color="blue" dark @click="showSignupDialog">계정없는 흑우 없제?</v-btn>
+            <v-spacer/>
             <v-btn color="red lighter-1" dark @click="cancelLogin">닫기</v-btn>
             <v-btn color="green lighter-1" dark @click="toLogin">로그인</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
+
+      <v-dialog v-model="signupDialog" max-width="600px">
+        <v-card>
+          <v-card-title class="headline">회원가입</v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12>
+                  <v-text-field v-model="name" label="이름" autofocus></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field v-model="id" label="아이디"></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field v-model="pw" label="비밀번호"
+                  :append-icon="pwToggle ? 'visibility' : 'visibility_off'"
+                  :type="pwToggle ? 'text' : 'password'"
+                  @click:append="pwToggle = !pwToggle"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn color="red lighter-1" dark @click="cancelSignup">닫기</v-btn>
+            <v-btn color="green lighter-1" dark @click="toSignup">가입</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
       <v-snackbar v-model="snackbar" :color="snackbarColor" :timeout="timeout" top>
         {{text}}
       </v-snackbar>
@@ -97,17 +139,22 @@ export default {
   name: 'App',
   data () {
     return {
+      name: '',
       id: '',
       pw: '',
       isAuth: false,
-      dialog: false,
+      logoutDialog: false,
+      loginDialog: false,
+      signupDialog: false,
       pwToggle: false,
       snackbar: false,
       timeout: 3000,
       snackbarColor: '',
-      text: 'IT IS TEST TEXT',
+      text: '',
+      failColor: 'rgba(255, 0, 0, 0.7)',
       account: {
-        'name': 'Jo Yeonghwan'
+        'name': '',
+        'id': ''
       },
       items: [
         {
@@ -124,42 +171,78 @@ export default {
     }
   },
   methods: {
-    goHome () {
-      this.$router.push('/')
-    },
     test () {
       console.log('it is test method!')
     },
     cancelLogin () {
-      this.dialog = false
+      this.loginDialog = false
       this.resetInput()
+    },
+    cancelSignup () {
+      this.signupDialog = false
+      this.resetInput()
+    },
+    toLogout () {
+      this.account = {}
+      this.isAuth = false
+      this.logoutDialog = false
+
+      this.snackbarColor = 'success'
+      this.text = '로그아웃 되었습니다'
+      this.snackbar = true
     },
     toLogin () {
       if (this.id === '' || this.pw === '') {
         console.log('ID or password is empty!')
-        this.snackbarColor = 'rgba(255, 0, 0, 0.7)'
+        this.snackbarColor = this.failColor
         this.text = 'ID 혹은 비밀번호가 비어있습니다'
         this.snackbar = true
         return
       }
 
-      console.log(this.id)
-      console.log(this.pw)
       // 성공 시
       // this.snackbarColor = 'success'
       // this.text = '로그인에 성공했습니다'
       // this.snackbar = true
-      // this.isAuthed = true
+      // this.isAuth = true
 
       // 실패 시
-      // this.snackbarColor = 'rgba(255, 0, 0, 0.7)'
+      // this.snackbarColor = this.failColor
       // this.text = '로그인에 실패했습니다'
       // this.snackbar = true
 
-      this.dialog = false
+      this.loginDialog = false
+      this.resetInput()
+    },
+    showSignupDialog () {
+      this.resetInput()
+      this.loginDialog = false
+      this.signupDialog = true
+    },
+    toSignup () {
+      if (this.name === '' || this.id === '' || this.pw === '') {
+        console.log('Name or ID or password is empty!')
+        this.snackbarColor = this.failColor
+        this.text = '비어있는 항목이 있습니다'
+        this.snackbar = true
+        return
+      }
+
+      // 성공 시
+      // this.snackbarColor = 'success'
+      // this.text = '성공적으로 가입됐습니다'
+      // this.snackbar = true
+
+      // 실패 시
+      // this.snackbarColor = this.failColor
+      // this.text = '회원가입에 실패했습니다'
+      // this.snackbar = true
+
+      this.signupDialog = false
       this.resetInput()
     },
     resetInput () {
+      this.name = ''
       this.id = ''
       this.pw = ''
     }
--- fe/src/views/menu.vue
+++ fe/src/views/menu.vue
@@ -91,7 +91,7 @@ export default {
   data () {
     return {
       page: 1,
-      pagelength:0,
+      pagelength: 0,
       dataPerPage: 4,
       like: 0,
       dislike: 0,
@@ -106,16 +106,16 @@ export default {
     this.getData()
   },
   computed: {
-    startOffset() {
-      return ((this.page-1)*this.dataPerPage);
+    startOffset () {
+      return ((this.page - 1) * this.dataPerPage)
     },
-    endOffset() {
-      return (this.startOffset + this.dataPerPage);
+    endOffset () {
+      return (this.startOffset + this.dataPerPage)
     },
-    numOfPages() {
-      return Math.ceil(this.comment.length / this.dataPerPage);
+    numOfPages () {
+      return Math.ceil(this.comment.length / this.dataPerPage)
     },
-    calData() {
+    calData () {
       return this.comment.slice(this.startOffset, this.endOffset)
     }
   },
@@ -129,10 +129,10 @@ export default {
           this.dislike = r.data.dislike
           this.date = r.data.frequency
           this.comment = r.data.comments
-          if(this.comment.length%this.dataPerPage==0) {
-            this.pagelength=this.comment.length/this.dataPerPage
-          }else {
-            this.pagelength=this.comment.length/this.dataPerPage+1
+          if (this.comment.length % this.dataPerPage === 0) {
+            this.pagelength = this.comment.length / this.dataPerPage
+          } else {
+            this.pagelength = this.comment.length / this.dataPerPage + 1
           }
         })
         .catch((e) => {
