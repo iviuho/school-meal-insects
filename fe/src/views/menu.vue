4fe6c85b612aa83fd62f96f6e706ea98e68b2c3c
Mangbo <whdudghks45629@gmail.com>
2019-07-07 10:24:57 +0900

    value -1
    오류 발견 account

--- fe/src/views/menu.vue
+++ fe/src/views/menu.vue
@@ -9,14 +9,14 @@
           <v-btn v-if="account.likes.includes(this.id)" icon large @click="postReq(id, 'like')" style="width:64px; height:64px;">
             <v-icon color="success" size="64">thumb_up</v-icon>
           </v-btn>
-          <v-btn v-else icon large @click="postReq(id, 'like')" style="width:64px; height:64px;">
+          <v-btn v-else-if="!account.likes.includes(this.id)" icon large @click="postReq(id, 'like')" style="width:64px; height:64px;">
             <v-icon size="64">thumb_up</v-icon>
           </v-btn>
           <p><strong>&nbsp;{{like}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></p>
           <v-btn v-if="account.dislikes.includes(this.id)" icon large @click="postReq(id, 'dislike')" style="width:64px; height:64px;">
             <v-icon color="error" size="64">thumb_down</v-icon>
           </v-btn>
-          <v-btn v-else icon large @click="postReq(id, 'dislike')" style="width:64px; height:64px;">
+          <v-btn v-else-if="!account.dislikes.includes(this.id)" icon large @click="postReq(id, 'dislike')" style="width:64px; height:64px;">
             <v-icon size="64">thumb_down</v-icon>
           </v-btn>
           <p><strong>&nbsp;{{dislike}}&nbsp;</strong></p>
@@ -143,19 +143,64 @@ export default {
     postReq (name, order) {
       if (this.isAuth) {
         const baseURI = 'http://localhost:3000/menu/'
-        if (!(this.account.likes.includes(this.id))) {
-          this.$http.post(baseURI + name, {
-            order: order
-          })
-            .then((r) => {
-              console.log(r)
-              this.$emit('changeData', this.id)
-              this.account.likes.push(this.id)
-              this.getData()
+        if(order=="like") {
+          if(!(this.account.likes.includes(this.id))) {
+            this.$http.post(baseURI + name, {
+              order: order
             })
-            .catch((e) => {
-              console.error(e.message)
+              .then((r) => {
+                console.log(r)
+                this.$emit('changeData', this.id, order)
+                this.getData()
+                console.log(this.account.likes)
+              })
+              .catch((e) => {
+                console.error(e.message)
+              })
+          }
+          else if(this.account.likes.includes(this.id)) {
+            this.$http.post(baseURI + name, {
+              order: order,
+              value: -1
             })
+              .then((r) => {
+                console.log(r)
+                this.$emit('exceptData', this.id, order)
+                this.getData()
+              })
+              .catch((e) => {
+                console.error(e.message)
+              })
+          }
+        }
+        else if(order=="dislike") {
+          if(this.account.dislikes.includes(this.id)) {
+            this.$http.post(baseURI + name, {
+              order: order,
+              value: -1
+            })
+              .then((r) => {
+                console.log(r)
+                this.$emit('exceptData', this.id, order)
+                this.getData()
+              })
+              .catch((e) => {
+                console.error(e.message)
+              })
+          }
+          else if(!(this.account.dislikes.includes(this.id))) {
+            this.$http.post(baseURI + name, {
+              order: order
+            })
+              .then((r) => {
+                console.log(r)
+                this.$emit('changeData', this.id, order)
+                this.getData()
+              })
+              .catch((e) => {
+                console.error(e.message)
+              })
+          }
         }
       }
     },
